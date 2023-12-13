d755528db5920c60d17af34035624f1ba148f8ea
rodionkasirin <k.rodion77@gmail.com>
2023-12-13 18:32:58 +0300

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY Users /code/
 
-EXPOSE 8000
+EXPOSE 80
 
 CMD ["uvicorn", "UsersService:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ Users/unit_tests.py
@@ -0,0 +1,15 @@
+
+from fastapi.testclient import TestClient
+from UsersApp.Users.UsersService import app
+
+client = TestClient(app)
+
+def test_get_users():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == "Allow users: id-1:User1; id-2:User2; id-3:User3"
+
+def test_get_cancel_users():
+    response = client.get('/cancel_users')
+    assert response.status_code == 200
+    assert response.json() == "Cancel users: id-4:User4; id-5:User5; id-6:User6"
